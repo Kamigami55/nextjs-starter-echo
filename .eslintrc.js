5f8d2f748956bdca367dac9ecb22d883ac495f00
Kamigami55 <eason@easonchang.com>
2022-04-29 03:11:22 +0800

    Remove redundant eslint config
    Remove configs that already defined in eslint-config-eason

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,6 @@
-// Mainly follow this tutorial for configuration
-// https://dev.to/heyitsarpit/eslint-and-prettier-for-react-apps-bonus-next-js-and-typescript-3e46
-
 module.exports = {
   root: true, // Make sure eslint picks up the config at the root of the directory
-  parserOptions: {
-    ecmaVersion: 'latest', // Use the latest ecmascript standard
-    sourceType: 'module', // Allows using import/export statements
-    ecmaFeatures: {
-      jsx: true, // Enable JSX since we're using React
-    },
-  },
   settings: {
-    react: {
-      version: 'detect', // Automatically detect the react version
-    },
     'import/resolver': {
       alias: {
         map: [['@', './src']],
@@ -21,25 +8,13 @@ module.exports = {
       },
     },
   },
-  env: {
-    browser: true, // Enables browser globals like window and document
-    amd: true, // Enables require() and define() as global variables as per the amd spec.
-    node: true, // Enables Node.js global variables and Node.js scoping.
-  },
   extends: [
     'eason',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
     'plugin:tailwindcss/recommended',
-    'plugin:prettier/recommended',
     'plugin:storybook/recommended',
   ],
-  plugins: ['simple-import-sort', 'tailwindcss'],
+  plugins: ['tailwindcss'],
   rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'warn',
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
--- src/constants/siteMeta.js
+++ src/constants/siteMeta.js
@@ -1,2 +1,3 @@
 export const SITE_TITLE = 'Next.js Starter'
-export const SITE_DESCRIPTION = 'Awesome Next.js starter template by Eason Chang'
+export const SITE_DESCRIPTION =
+  'Awesome Next.js starter template by Eason Chang'
