c2de5f416679952508c5c907c2826d834af199d8
Kamigami55 <eason@easonchang.com>
2022-04-30 01:09:04 +0800

    Update next.config.js
    - Enable SWC minify
    - Ignore eslint error when build
    - Enable NextImage avif format

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  // SWC minification for build performance
+  // https://nextjs.org/docs/advanced-features/compiler#minification
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    // Enable AVIF support
+    formats: ['image/avif', 'image/webp'],
+  },
 }
